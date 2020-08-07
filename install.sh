7fc14e047d823bfdf1ac40d886df58c4e71deda3
fhamm <felipehammcwb@hotmail.com>
2020-08-07 15:32:49 -0300

    Updated install script

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
+#!/bin/bash 
+
+INSTALLER_PATH=$(readlink -f $0)
+INSTALLER_DIRECTORY=$(dirname "${INSTALLER_PATH}")
+MAIN_PATH=$INSTALLER_DIRECTORY/src/main.py
+
 chmod +x src/main.py
 
-ln -s $HOME/kyoto/src/main.py /usr/local/bin/kyoto
+ln -s $MAIN_PATH /usr/local/bin/kyoto
